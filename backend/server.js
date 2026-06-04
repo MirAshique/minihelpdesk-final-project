578da35217c6d4b4e525254e0cab57a86306c78c
Mir Ashique Hussain Talpur <ashiqtalpur18@gmail.com>
2026-06-04 11:21:59 +0500

    Add environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
@@ -6,7 +9,7 @@ app.get("/", (req, res) => {
   res.send("MiniHelpDesk API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
