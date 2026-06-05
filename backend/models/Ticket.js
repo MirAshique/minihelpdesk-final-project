e05c861dc04e53cca2a018924e47731159c664ba
Mir Ashique Hussain Talpur <ashiqtalpur18@gmail.com>
2026-06-05 14:33:05 +0500

    Create ticket model

---
+++ backend/models/Ticket.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = new mongoose.Schema(
+  {
+    subject: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    status: {
+      type: String,
+      enum: ["Open", "In Progress", "Closed"],
+      default: "Open",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Ticket", ticketSchema);
